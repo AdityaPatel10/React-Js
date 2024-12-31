e49d4d273ef4d6f4e1f4013ccd72fbb34033d9ee
AdityaPatel10 <aditya97556@gmail.com>
2024-12-31 14:55:26 +0530

    Adding basic-reaact

---
+++ 01basic-react/src/App.js
@@ -0,0 +1,12 @@
+import Trial from "./Trial";
+
+function App() {
+  return (
+    <>
+   <h1>Hello World!</h1>
+   <Trial />
+    </>
+  );
+}
+
+export default App;
---
+++ 01basic-react/src/Trial.js
@@ -0,0 +1,9 @@
+function Trial(){
+    return(
+        <h3>
+            This is a trial component.
+        </h3>
+    )
+}
+
+export default Trial;
